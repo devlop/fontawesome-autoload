511d246fe7a7d49d15daddefbd5c2c20acfa3eb9
Johan <johan@devlop.se>
2021-03-17 19:11:11 +0700

    first simple test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,12 @@
-const LaravelMixManifestWebpackPlugin = require('../src/index');
+const FontAwesomeAutoloadWebpackPlugin = require('../src/index');
 const path = require('path');
 
-describe('laravel-mix-manifest-webpack-plugin', () => {
+describe('fontawesome-autoload-webpack-plugin', () => {
     it('can be instantiated', () => {
-        expect(new LaravelMixManifestWebpackPlugin({
-            public: path.resolve(__dirname, './'),
-            name: 'mix-manifest.json',
-        })).toBeInstanceOf(LaravelMixManifestWebpackPlugin);
+        expect(new FontAwesomeAutoloadWebpackPlugin({
+            paths: [
+                path.resolve(__dirname, './'),
+            ],
+        })).toBeInstanceOf(FontAwesomeAutoloadWebpackPlugin);
     });
 });
